f78e20702e8575bbecad49c0130e0d3801d8b3c5
Michael Casile <mikec711g@gmail.com>
2026-03-25 14:13:45 -0400

    Modifications to CSV creator

--- scripts/cre8CsvsFromJson.js
+++ scripts/cre8CsvsFromJson.js
@@ -1,6 +1,32 @@
 taxCatList = ['BE', 'BI', 'CE' ] ;
 houseList = ['107EI', '133RD', '187NP', '189NP', '251PB', '259PB', '29EG',
     '33PP', '395WW', '483ES', '495ES', '502S13', '502WD', '632CB', '805PB', '8416SC' ]
+catMap = new Map([
+    ['Administrative Expenses', 'Administrative Expenses'],
+    ['Building Supplies', 'Building Supplies'],
+    ['Electric', 'Electric'],
+    ['Garbage', 'Trash and Recycling'],
+    ['Gas & Propane', 'Gas'],
+    ['Labor', 'Labor'],
+    ['Mortgage Interest', 'Mortgage Interest'],
+    ['Pest Control', 'Pest Control'],
+    ['Property Insurance', 'Insurance'],
+    ['Property Taxes', 'Property Tax'],
+    ['Rent Income', 'Rent Income'],
+    ['Tools and Supplies', 'Tools and Supplies'],
+    ['Water & Sewer', 'Water and Sewer']    
+]) ;
+tabMap = new Map([
+    ['OfficeNBusinessExpenses', ['Administrative Expenses']],
+    ['BuildingSupplies', ['Building Supplies']],
+    ['Utilities', ['Electric', 'Trash and Recycling', 'Gas', 'Water and Sewer']],
+    ['Labor', ['Labor']],
+    ['PestControl', ['Pest Control']],
+    ['Insurance', ['Insurance']],
+    ['PropertyTax', ['Property Tax']],
+    ['Income', ['Rent Income']], 
+    ['ToolsNSupplies', ['Tools and Supplies']] 
+]) ;
 
 function startEventListener() {
     const fileSelector = document.getElementById("FileSelector");
@@ -26,9 +52,9 @@ function procJson(jsonData) {
     const filtTrans = jsonData.
         filter(tran => taxCatList.includes(tran.TaxCat) && houseList.includes(tran.House)).
         map((({ House, TranDate, Amount, Category, TranExtra, Annotation }) => 
-            ({ House, TranDate, Amount, Category, TranExtra, Annotation }))).
+            ({ House, TranDate, Amount, Category: catMap.get(Category), TranExtra, Annotation }))).
         sort((a, b) => {
-            let cmp0 = a.Category.localeCompare(b.Category) ;
+            let cmp0 = (!a.Category || a.Category.localeCompare(b.Category)) ;
             if (cmp0 !== 0)  return cmp0 ; 
             else {
                 cmp0 = a.House.localeCompare(b.House) ;
@@ -36,45 +62,48 @@ function procJson(jsonData) {
                 else return a.TranDate.localeCompare(b.TranDate) ;
             }
         }) ;
-    prepFile(filtTrans, "cre.csv") ;
+    arr2csv(filtTrans) ;
 }
 
 function arr2csv(jsonArr) {
-    let csv = 'House,TranDate,Amount,SubTotal,Category,TranExtra,Annotation\n' ;
-    let curCat = jsonArr[0].Category ;    let curHouse = jsonArr[0].House ;
-    jsonArr.forEach(row => {
-        if (row.Category !== curCat) {
-            csv += writeBlnkCsvRow() ;    csv += writeBlnkCsvRow() ;
-            curCat = row.Category ;
-        } else if (row.House !== curHouse) {
-            csv += writeBlnkCsvRow() ;
-            curHouse = row.House ;
-        }
-        if (row.TranExtra.includes(','))  row.TranExtra = `"${row.TranExtra}"` ;
-        if (row.Annotation.includes(','))  row.Annotation = `"${row.Annotation}"` ;
-        csv += `${row.House},${row.TranDate},${row.Amount}, ,${row.Category},${row.TranExtra},${row.Annotation}\n` ;
-    })
-    return csv ;
+    let headerStr = 'House,TranDate,Amount,SubTotal,Category,TranExtra,Annotation\n' ;   // For categories not in structure
+    const newCat1 = ', , , , , , \n , , , ,' ; const newCat2 = ' , , \n' ;
+    const newHouseStr = ', , , , , , \n';
+    let timeDelay = 1 ;
+    for (let foldEnt of tabMap.entries()) {   // For each folder in structure, add folder row to appropriate string and add blank row for category rows
+        const foldName = foldEnt[0] ;
+        const catArr = foldEnt[1] ;
+        const foldArr = jsonArr.filter(row => catArr.includes(row.Category)) ;
+        console.log('foldName: %s  catArr: %o  foldArr len: %d', foldName, catArr, foldArr.length) ;
+        if (foldArr.length === 0)  continue ;
+        let curCat = '' ;    let curHouse = '' ;  let csvStr = headerStr
+        foldArr.forEach(row => {
+            if (row.Category !== curCat) {
+                if (curCat !== '')  csvStr += newCat1 + row.Category + newCat2 ;
+                curCat = row.Category ;
+            } else if (row.House !== curHouse) {
+                csvStr += newHouseStr ;
+                curHouse = row.House ;
+            }
+            if (row.TranExtra.includes(','))  row.TranExtra = `"${row.TranExtra}"` ;
+            if (row.Annotation.includes(','))  row.Annotation = `"${row.Annotation}"` ;
+            csvStr += `${row.House},${row.TranDate},${row.Amount}, ,${row.Category},${row.TranExtra},${row.Annotation}\n` ;
+        })
+        const encodedUri = "data:text/csv;charset=utf-8," + encodeURIComponent(csvStr) ;
+        writeFile(encodedUri, timeDelay++, foldName + '.csv') ;
+    }
 }
 
-function writeBlnkCsvRow() {
-    return ', , , , , , \n' ;
-}
-
-function prepFile(jsonArr, outName) {
-    const csv = arr2csv(jsonArr) ;
-    const encodedUri = "data:text/csv;charset=utf-8," + encodeURIComponent(csv) ;
-    writeFile(encodedUri, outName) ;
-}
-
-function writeFile(encodedData, fileName) {
-    console.log('writeFile filenm: %s', fileName)
+function writeFile(encodedData, timeDelay, fileName) {
+    setTimeout(() => {
+    console.log('writeFile filenm: %s  len: %d  delay: %d', fileName, encodedData.length, timeDelay)
     const dlAnchor = document.createElement('a')
     dlAnchor.setAttribute("href", encodedData)
     dlAnchor.setAttribute("download", fileName)
     document.body.appendChild(dlAnchor)
     dlAnchor.click()
     dlAnchor.remove()
+    }, timeDelay*1000);
 }
 
 startEventListener() ;
